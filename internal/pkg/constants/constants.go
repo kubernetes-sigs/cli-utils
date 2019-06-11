fdb11170a0f0b079778778fcfb56c6529eb176e3
jingfangliu <jingfangliu@google.com>
2019-06-11 09:11:10 -0700

    add comments

--- internal/pkg/constants/constants.go
+++ internal/pkg/constants/constants.go
@@ -18,6 +18,25 @@ package constants
 
 const (
 	Presence = "kubectl.kubernetes.io/presence"
+
+	// Any resource with the annotation
+	//   kubectl.kubernetes.io/presence: EnsureExist
+	// will not be pruned or deleted.
+	//
+	// It following effect in each command
+	// - no effect in apply
+	// - prune skips this resource
+	// - delete skips this resource
+
 	EnsureExist = "EnsureExist"
+
+	// Any resource with the annotation
+	//  kubectl.kubernetes.io/presence: EnsureDoesNotExist
+	// Will be deleted or skipped in apply.
+	//
+	// It has following effect in each command
+	// - the resource is skipped in apply
+	// - the resource is deleted in prune
+	// - the resource is deleted in delete
 	EnsureNoExist = "EnsureDoesNotExist"
 )
