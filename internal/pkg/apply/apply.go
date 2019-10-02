26d21542e2ddd1a4fe4fffa8974020c1740ba614
Sean Sullivan <seans@google.com>
2019-10-02 16:46:15 -0700

    Uses OwnerReference to manage inventory, apply, and pruning

--- internal/pkg/apply/apply.go
+++ internal/pkg/apply/apply.go
@@ -17,10 +17,12 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"sigs.k8s.io/cli-utils/internal/pkg/constants"
+	"strconv"
+	"time"
 
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/cli-utils/internal/pkg/client"
@@ -43,6 +45,8 @@ type Apply struct {
 
 	// Commit is a git commit object
 	Commit *object.Commit
+
+	// TODO: add prune and dry-run flags here
 }
 
 // Result contains the Apply Result
@@ -57,74 +61,142 @@ func (a *Apply) Do() (Result, error) {
 	// TODO(Liuijngfang1): add a dry-run for all objects
 	// When the dry-run passes, proceed to the actual apply
 
+	var director *unstructured.Unstructured
+	var inventoryRef *metav1.OwnerReference
 	for _, u := range normalizeResourceOrdering(a.Resources) {
 		if util.HasAnnotation(u, inventory.ContentAnnotation) {
-			var err error
-			u, err = a.updateInventoryObject(u)
-			if err != nil {
-				fmt.Fprintf(a.Out, "failed to update inventory object %v\n", err)
+			// Validate that i == 0
+			if director == nil {
+				director = u.DeepCopy()
+				err := a.DynamicClient.Get(context.Background(),
+					types.NamespacedName{Namespace: u.GetNamespace(), Name: u.GetName()}, director)
+				if err != nil && !errors.IsNotFound(err) {
+					fmt.Fprintf(a.Out, "Failure during inventory director search: %v\n", err)
+					break
+				}
+				if err != nil && errors.IsNotFound(err) {
+					director = &unstructured.Unstructured{}
+					director.SetGroupVersionKind(u.GroupVersionKind())
+					director.SetNamespace(u.GetNamespace())
+					director.SetName(u.GetName())
+					err = a.DynamicClient.Create(context.Background(), director, &metav1.CreateOptions{})
+					if err != nil {
+						fmt.Fprintf(a.Out, "Failed to create inventory director: %v\n", err)
+						break
+					}
+					fmt.Fprintf(a.Out, "Created inventory director: %s/%s\n", director.GetKind(), u.GetName())
+				}
 			}
+			directorRef := createOwnerReference(director)
+			a.addOwnerReference(u, *directorRef)
+			suffix := createUniqueId()
+			u.SetName(u.GetName() + "-" + suffix)
+			// director.Items = append([]unstructured.Unstructured{*u}, director.Items...)
+		} else if inventoryRef != nil {
+			a.addOwnerReference(u, *inventoryRef)
 		}
-		if util.MatchAnnotations(u, map[string]string{
-			constants.Presence: constants.EnsureNoExist,
-		}) {
-			// not applying the resource
-			continue
-		}
+
 		err := a.DynamicClient.Apply(context.Background(), u)
 		if err != nil {
 			fmt.Fprintf(a.Out, "failed to apply the object: %s/%s: %v\n", u.GetKind(), u.GetName(), err)
 			continue
 		}
 		fmt.Fprintf(a.Out, "applied %s/%s\n", u.GetKind(), u.GetName())
+
+		// Create after applying, so the UID field is included.
+		if util.HasAnnotation(u, inventory.ContentAnnotation) {
+			inventoryRef = createOwnerReference(u)
+		}
 	}
 	return Result{Resources: a.Resources}, nil
 }
 
-func (a Apply) updateInventoryObject(u *unstructured.Unstructured) (*unstructured.Unstructured, error) {
-	obj := u.DeepCopy()
-	err := a.DynamicClient.Get(context.Background(),
-		types.NamespacedName{Namespace: u.GetNamespace(), Name: u.GetName()}, obj)
-	if err != nil && !errors.IsNotFound(err) {
-		return nil, err
-	}
-	if errors.IsNotFound(err) {
-		return u, nil
-	}
-
-	oldAnnotation := obj.GetAnnotations()
-	newAnnotation := u.GetAnnotations()
-	oldhash, okold := oldAnnotation[inventory.HashAnnotation]
-	newhash, oknew := newAnnotation[inventory.HashAnnotation]
-	if okold && oknew && oldhash == newhash {
-		return obj, nil
-	}
-
-	return mergeContentAnnotation(u, obj)
+// createUniqueId creates a string from the current Unix time.
+func createUniqueId() string {
+	// TODO: probably change this to a random number
+	nanoSeconds := time.Now().UnixNano()
+	return strconv.FormatInt(nanoSeconds, 10) // base 10
 }
 
-func mergeContentAnnotation(newObj, oldObj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
-	newInv := inventory.NewInventory()
-	err := newInv.LoadFromAnnotation(newObj.GetAnnotations())
-	if err != nil {
-		return nil, err
-	}
-	oldInv := inventory.NewInventory()
-	err = oldInv.LoadFromAnnotation(oldObj.GetAnnotations())
-	if err != nil {
-		return nil, err
-	}
-	newInv.Previous.Merge(oldInv.Previous)
-	newInv.Previous.Merge(oldInv.Current)
+var falsePtr = false
 
-	annotations := newObj.GetAnnotations()
-	err = newInv.UpdateAnnotations(annotations)
-	if err != nil {
-		return nil, err
+func createOwnerReference(u *unstructured.Unstructured) *metav1.OwnerReference {
+	if u == nil {
+		return nil
 	}
-	newObj.SetAnnotations(annotations)
-	return newObj, nil
+	// TODO: Validate the owner referent
+	// TODO: Print warning if UID is empty (happens in dry-run)
+	return &metav1.OwnerReference{
+		APIVersion:         u.GetAPIVersion(),
+		Kind:               u.GetKind(),
+		Name:               u.GetName(),
+		UID:                u.GetUID(),
+		Controller:         &falsePtr,
+		BlockOwnerDeletion: &falsePtr, // We never want to block in foreground deletion
+	}
 }
+
+func (a *Apply) addOwnerReference(u *unstructured.Unstructured, ownerRef metav1.OwnerReference) {
+	ownerReferences := u.GetOwnerReferences()
+	// Check if the OwnerReference is already in the list.
+	found := false
+	for _, existingRef := range ownerReferences {
+		if ownerRef.UID == existingRef.UID {
+			found = true
+			break
+		}
+	}
+	// Only add the OwnerReference if it doesn't already exist in the current list.
+	if !found {
+		ownerReferences = append(ownerReferences, ownerRef)
+		u.SetOwnerReferences(ownerReferences)
+	}
+}
+
+// func (a Apply) updateInventoryObject(u *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+// 	obj := u.DeepCopy()
+// 	err := a.DynamicClient.Get(context.Background(),
+// 		types.NamespacedName{Namespace: u.GetNamespace(), Name: u.GetName()}, obj)
+// 	if err != nil && !errors.IsNotFound(err) {
+// 		return nil, err
+// 	}
+// 	if errors.IsNotFound(err) {
+// 		return u, nil
+// 	}
+
+// 	oldAnnotation := obj.GetAnnotations()
+// 	newAnnotation := u.GetAnnotations()
+// 	oldhash, okold := oldAnnotation[inventory.HashAnnotation]
+// 	newhash, oknew := newAnnotation[inventory.HashAnnotation]
+// 	if okold && oknew && oldhash == newhash {
+// 		return obj, nil
+// 	}
+
+// 	return mergeContentAnnotation(u, obj)
+// }
+
+// func mergeContentAnnotation(newObj, oldObj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+// 	newInv := inventory.NewInventory()
+// 	err := newInv.LoadFromAnnotation(newObj.GetAnnotations())
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	oldInv := inventory.NewInventory()
+// 	err = oldInv.LoadFromAnnotation(oldObj.GetAnnotations())
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	newInv.Previous.Merge(oldInv.Previous)
+// 	newInv.Previous.Merge(oldInv.Current)
+
+// 	annotations := newObj.GetAnnotations()
+// 	err = newInv.UpdateAnnotations(annotations)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	newObj.SetAnnotations(annotations)
+// 	return newObj, nil
+// }
 
 // normalizeResourceOrdering moves the inventory object to be the first resource
 func normalizeResourceOrdering(resources clik8s.ResourceConfigs) []*unstructured.Unstructured {
