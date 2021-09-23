4f4bd30c57094d48dcf6e8967db35440443eb2b0
Karl Isenberg <karlisenberg@google.com>
2021-09-23 16:58:19 -0700

    Change YamlStringer to only accept Unstructured
    - This avoids a runtime schema dependency

--- pkg/object/strings.go
+++ pkg/object/strings.go
@@ -7,29 +7,21 @@ import (
 	"fmt"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/kubectl/pkg/scheme"
 	"sigs.k8s.io/yaml"
 )
 
-var codec = scheme.Codecs.LegacyCodec(scheme.Scheme.PrioritizedVersionsAllGroups()...)
-
 // YamlStringer delays YAML marshalling for logging until String() is called.
 type YamlStringer struct {
-	O runtime.Object
+	O *unstructured.Unstructured
 }
 
 // String marshals the wrapped object to a YAML string. If serializing errors,
 // the error string will be returned instead. This is primarily for use with
 // verbose logging.
 func (ys YamlStringer) String() string {
-	jsonBytes, err := runtime.Encode(unstructured.NewJSONFallbackEncoder(codec), ys.O)
+	yamlBytes, err := yaml.Marshal(ys.O)
 	if err != nil {
-		return fmt.Sprintf("<<failed to serialize as json: %s>>", err)
-	}
-	yamlBytes, err := yaml.JSONToYAML(jsonBytes)
-	if err != nil {
-		return fmt.Sprintf("<<failed to convert from json to yaml: %s>>", err)
+		return fmt.Sprintf("<<failed to serialize as yaml: %s>>", err)
 	}
 	return string(yamlBytes)
 }
