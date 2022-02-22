2a111beb8f52182c44808e2925671d722ac11c45
Karl Isenberg <karlisenberg@google.com>
2022-02-22 13:57:17 -0800

    chore: Add example test summary
    - Install column when running example tests in prow

--- hack/testExamplesE2EAgainstKapply.sh
+++ hack/testExamplesE2EAgainstKapply.sh
@@ -11,7 +11,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-succeeded=0
+results=()
 failed=0
 
 function run_test() {
@@ -27,19 +27,29 @@ for path in examples/alphaTestExamples/*.md; do
     if run_test "${path}"; then
         echo
         echo -e "${GREEN}Example Test Succeeded: ${test_name}${NC}"
-        let "succeeded+=1"
+        results+=("${test_name}\t${GREEN}Succeeded${NC}")
     else
         echo
         echo -e "${RED}Example Test Failed: ${test_name}${NC}"
         let "failed+=1"
+        results+=("${test_name}\t${RED}Failed${NC}")
     fi
   echo
 done
 
+(
+    echo -e "TEST\tRESULT"
+    for result in "${results[@]}"; do
+        echo -e "${result}"
+    done
+) | column -t
+
+echo
+
 if [[ ${failed} -gt 0 ]]; then
-    echo -e "${RED}Example Tests Complete (succeeded: ${succeeded}, failed: ${failed})${NC}"
+    echo -e "${RED}Example Tests Failed${NC}"
     exit 1
 else
-    echo -e "${GREEN}Example Tests Complete (succeeded: ${succeeded}, failed: ${failed})${NC}"
+    echo -e "${GREEN}Example Tests Succeeded${NC}"
     exit 0
 fi
