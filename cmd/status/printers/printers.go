99d35d57cdf3d1ca1fdb152414c5c920eb4ccf44
Johnnie Chou <johnniechou@google.com>
2022-08-08 21:57:26 +0000

    add struct PrintData to add inventory name and namespace info in printed text and modify test cases to current expected output

--- cmd/status/printers/printers.go
+++ cmd/status/printers/printers.go
@@ -4,20 +4,19 @@
 package printers
 
 import (
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"sigs.k8s.io/cli-utils/cmd/status/printers/event"
 	"sigs.k8s.io/cli-utils/cmd/status/printers/printer"
 	"sigs.k8s.io/cli-utils/cmd/status/printers/table"
-
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
 // CreatePrinter return an implementation of the Printer interface. The
 // actual implementation is based on the printerType requested.
-func CreatePrinter(printerType string, ioStreams genericclioptions.IOStreams) (printer.Printer, error) {
+func CreatePrinter(printerType string, ioStreams genericclioptions.IOStreams, printData *printer.PrintData) (printer.Printer, error) {
 	switch printerType {
 	case "table":
 		return table.NewPrinter(ioStreams), nil
 	default:
-		return event.NewPrinter(ioStreams), nil
+		return event.NewPrinter(ioStreams, printData), nil
 	}
 }
