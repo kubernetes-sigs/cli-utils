64c8ce4a058e0b2b1d95113e93efdee1755a87d1
Sam Dowell <sdowell@google.com>
2025-01-31 15:13:37 -0800

    feat: rewrite inventory client with basic CRUD
    This change rewrites the inventory client interface using simple CRUD
    methods. Prior to this change the inventory client consisted of several
    tightly coupled interfaces which made the client more complicated and
    less flexible.
    
    The old client also performed a Get of the inventory many times
    throughout the applier/destroyer processes, including before every
    update. This approach discarded optimistic concurrency protection since
    the resourceVersion was never persisted. The new approach preserves an
    internal interface representation of the inventory so that the
    resourceVersion can be preserved across updates.
    
    The new client interface is simpler to implement and is intended to
    support implementations for sharded inventories, which may consist of
    multiple underlying KRM objects. The prior interfaces made assumptions
    of a single KRM object in several places, which made it incompatible
    with a sharded inventory.

--- pkg/apply/destroyer_test.go
+++ pkg/apply/destroyer_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/cli-utils/pkg/apply/event"
-	"sigs.k8s.io/cli-utils/pkg/inventory"
 	pollevent "sigs.k8s.io/cli-utils/pkg/kstatus/polling/event"
 	"sigs.k8s.io/cli-utils/pkg/kstatus/status"
 	"sigs.k8s.io/cli-utils/pkg/object"
@@ -314,15 +313,13 @@ func TestDestroyerCancel(t *testing.T) {
 		t.Run(tn, func(t *testing.T) {
 			statusWatcher := newFakeWatcher(tc.statusEvents)
 
-			invInfo, err := tc.invInfo.toWrapped()
-			require.NoError(t, err)
-			cm, err := inventory.InvInfoToConfigMap(invInfo)
+			invInfo, err := tc.invInfo.toInfo()
 			require.NoError(t, err)
 
 			destroyer := newTestDestroyer(t,
 				tc.invInfo,
 				// Add the inventory to the cluster (to allow deletion)
-				append(tc.clusterObjs, cm),
+				append(tc.clusterObjs, tc.invInfo.toUnstructured()),
 				statusWatcher,
 			)
 
